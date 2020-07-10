65f4fb1baa7266e7d38176be8b9764848473ab6e
Goran Krampe <goran.krampe@gmail.com>
2020-07-10 11:21:23 +0000

    Migration

---
+++ migrations/00000010_unnamed.migration.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:aqueduct/aqueduct.dart';   
+
+class Migration10 extends Migration { 
+  @override
+  Future upgrade() async {
+   		database.addColumn("_Proof", SchemaColumn.relationship("sample", ManagedPropertyType.bigInteger, relatedTableName: "_Sample", relatedColumnName: "id", rule: DeleteRule.nullify, isNullable: true, isUnique: true));
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
